4b5b36c08c9e3bf401185e47381183fada082c56
HoangT <ac5217@student.jamk.fi>
2022-08-22 08:59:06 +0300

    clean the codebase

--- index.js
+++ index.js
@@ -1,71 +1,19 @@
-const hero = {
-  elementId: "hero",
-  name: "Wizard",
-  avatar: "images/wizard.png",
-  health: 60,
-  diceRoll: [3, 1, 4],
-  diceCount: 3,
-};
+import characterData from "./data.js";
+import { Character } from "./Charater.js";
 
-const monster = {
-  elementId: "monster",
-  name: "Orc",
-  avatar: "images/orc.png",
-  health: 10,
-  diceRoll: [6],
-  diceCount: 1,
-};
+const wizard = new Character(characterData.monster);
+console.log(characterData.monster);
+const orc = new Character(characterData.hero);
 
-function Character(data) {
-  this.elementId = data.elementId;
-  this.name = data.name;
-  this.avatar = data.avatar;
-  this.health = data.health;
-  this.diceRoll = data.diceRoll;
-  this.diceCount = data.diceCount;
-  this.getCharactorHtml = function () {
-    document.getElementById(
-      this.elementId
-    ).innerHTML = `<div class="character-card">
-            <h4 class="name"> ${this.name} </h4>
-            <img class="avatar" src="${this.avatar}" />
-            <div class="health">health: <b> ${this.health} </b></div>
-            <div class="dice-container">
-            ${getDiceHtml(this.diceCount)}
-            </div>
-        </div>`;
-  };
+/* MINI CHALLENGE */
+// 1 Create one render() function that calls both wizard and orc
+//  so we can control when they render.
+
+function render() {
+  document.getElementById("hero").innerHTML = wizard.getCharacterHtml();
+  document.getElementById("monster").innerHTML = orc.getCharacterHtml();
 }
 
-const wizard = new Character(hero);
-const orc = new Character(monster);
-wizard.getCharactorHtml();
-orc.getCharactorHtml();
+render();
 
-//GetRandom
-/*
-Challenge
-1. Create a function called getDiceRollArray that uses a 
-   for loop to return an array of random numbers between 1-6. 
-2  The function should have diceCount as a parameter and the 
-   array it returns should be diceCount length.
-3  For testing purposes, call the function with a diceCount of 
-   3 and log out the result. 
-** check out hint.md for extra help! **
-*/
-
-function getDiceRollArray(diceCount) {
-  return new Array(diceCount).fill(0).map(function () {
-    return Math.floor(Math.random() * 6 + 1);
-  });
-}
-
-console.log(getDiceRollArray(3));
-
-function getDiceHtml(diceCount) {
-  return getDiceRollArray(diceCount)
-    .map(function (num) {
-      return `<div class="dice">${num}</div>`;
-    })
-    .join("");
-}
+// 2 call the function.
